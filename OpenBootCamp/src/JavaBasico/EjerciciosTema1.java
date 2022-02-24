3f2607bb38af615657b885d2cded9d9b279ed9de
rhdario <82853235+rhdario@users.noreply.github.com>
2022-02-24 14:51:44 -0300

    Ejercicios Tema1
    Resolucion Ejercicios Tema 1 Open BootCamp - JAVA BASICO

---
+++ OpenBootCamp/src/JavaBasico/EjerciciosTema1.java
@@ -0,0 +1,49 @@
+package JavaBasico;
+
+public class EjerciciosTema1 {
+    public static void main(String[] args) {
+
+       //Enteros
+       byte numberbyte =1;
+       short numbershort = 2;
+       int numberint = 3;
+       long numberlong = 4;
+
+       System.out.println(numberbyte);
+       System.out.println(numbershort);
+       System.out.println(numberint);
+       System.out.println(numberlong);
+
+       //Punto Flotante
+       float decimalfloat = 4.9f;
+       double decimaldouble = 9.99d;
+
+       System.out.println(decimalfloat);
+       System.out.println(decimaldouble);
+
+       //Booleanos
+       boolean valorverdadero = true;
+       boolean valorfalso = false;
+
+       System.out.println(valorverdadero);
+       System.out.println(valorfalso);
+
+
+       //Cadenas de Texto
+       String nombre = "Hernan";
+
+       System.out.println(nombre);
+
+        //Caracter
+        char caracter1 = 'a';
+
+        System.out.println(caracter1);
+
+
+
+
+
+
+    }
+
+}
